ae67d7059bbb47cf7cdf2f9b1695e7343275b8d8
Venzor Arias Fabian <fabian_venzor@madisa.com>
2020-08-06 12:46:49 -0600

    Nuevos archivos para las ventanas de inicio

---
+++ Administrador.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace Pays_las_primas
+{
+    partial class Administrador
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbltext1 = new System.Windows.Forms.Label();
+            this.lbltext2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbltext1
+            // 
+            this.lbltext1.AutoSize = true;
+            this.lbltext1.Location = new System.Drawing.Point(33, 21);
+            this.lbltext1.Name = "lbltext1";
+            this.lbltext1.Size = new System.Drawing.Size(0, 13);
+            this.lbltext1.TabIndex = 0;
+            // 
+            // lbltext2
+            // 
+            this.lbltext2.AutoSize = true;
+            this.lbltext2.Location = new System.Drawing.Point(36, 60);
+            this.lbltext2.Name = "lbltext2";
+            this.lbltext2.Size = new System.Drawing.Size(70, 13);
+            this.lbltext2.TabIndex = 1;
+            this.lbltext2.Text = "Administrador";
+            // 
+            // Administrador
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 331);
+            this.Controls.Add(this.lbltext2);
+            this.Controls.Add(this.lbltext1);
+            this.Name = "Administrador";
+            this.Text = "Bienvenido Adminitstrador";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbltext1;
+        private System.Windows.Forms.Label lbltext2;
+    }
+}
---
+++ Administrador.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pays_las_primas
+{
+    public partial class Administrador : Form
+    {
+      
+        public Administrador(string nombre)
+        {
+            InitializeComponent();
+            lbltext1.Text = nombre;
+
+        }
+    }
+}
